01d87f161bc34c129d0fce8ab2d39dbe91f73c09
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-23 20:56:55 -0700

    learning objects

---
+++ js/object.js
@@ -0,0 +1,23 @@
+// object data type
+var obj = {}; // this is an empty object
+var car ={
+    make: 'honda',
+    model: 'civic',
+    year: '2019'
+};
+console.log(car);
+console.log(car.model);
+console.log(car.year);
+
+var date = {};// add date, month, year
+var date = {
+    date: 23,
+    month: 4,
+    year: 2019,
+    day: 'tuesday',
+
+}//display tuesday on our console
+
+console.log(date.day);
+
+
