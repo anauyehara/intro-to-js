d63c98af1336af533915df20bb2602696ddc8f98
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-24 19:47:12 -0700

    2nd part review-numbers

---
+++ js/review-data-types-1.js
@@ -0,0 +1,95 @@
+var int1 = 5;
+console.log(int1);
+var int2 = 10;
+console.log(int2);
+var int3 = 15;
+console.log(int3);
+var int4 = 20;
+console.log(int4);
+var int5 = 25;
+console.log(int5);
+
+
+var str1 = '5 dogs';
+console.log(str1);
+var str2 = '10 numbers';
+console.log(str2);
+var str3 = '15 candy bars';
+console.log(str3);
+var str4 = '20 quarters';
+console.log(str4);
+var str5 = '25 people';
+console.log(str5);
+
+var yes = true
+var no = false
+
+var beers =['stella','corona']; 
+console.log('beers');
+console.log(beers [0]); 
+console.log(beers [1]);
+
+var candybars =['twix','butterfinger']; 
+console.log('candybars');
+console.log(candybars [0]); 
+console.log(candybars [1]);
+
+var colors =['blue','pink']; 
+console.log('colors');
+console.log(colors [0]); 
+console.log(colors [1]);
+
+var phone =['iphone','android']; 
+console.log('phone');
+console.log(phone [0]); 
+console.log(phone [1]);
+
+var kids =['Isaiah','Keira']; 
+console.log('kids');
+console.log(kids [0]); 
+console.log(kids [1]);
+
+var Baseball_player ={
+    team: 'Dodgers',
+    player: 'Cody Bellinger',
+    position: 'first baseman',
+};
+console.log(Baseball_player);
+console.log(Baseball_player.team);
+console.log(Baseball_player.player);
+
+var football_player ={
+    team: 'Denver',
+    player: 'Von Miller',
+    number: '59',
+};
+console.log(football_player);
+console.log(football_player.player);
+console.log(football_player.number);
+
+var car ={
+    make: 'Lexus',
+    model: 'NX',
+    year: '2017',
+};
+console.log(car);
+console.log(car.make);
+console.log(car.model);
+
+var pizza ={
+    brand: 'me n eds',
+    Topings: 'pep and mushrooms',
+    curst: 'thin',
+};
+console.log(pizza);
+console.log(pizza.brand);
+console.log(pizza.Topings);
+
+var friends ={
+    friend1: 'Mari',
+    friend2: 'Keila',
+    friend3: 'Dana',
+};
+console.log(friends);
+console.log(friends.friend1);
+console.log(friends.friend2);
---
+++ js/review-number.js
@@ -0,0 +1,17 @@
+var d = 20;
+console.log(d);
+
+var g = 40;
+console.log(g);
+
+var mult= d*g;
+console.log(mult);
+
+var div = d/g;
+console.log(div);
+
+var mod= d % g;
+console.log(mod);
+
+var mod = g * g;
+console.log(mod);
