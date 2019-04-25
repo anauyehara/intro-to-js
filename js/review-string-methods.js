3c9ed927bd68060aea775054368c9928150472e1
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-24 20:39:53 -0700

    review string hello world

---
+++ js/review-string-methods.js
@@ -0,0 +1,15 @@
+var str1 = 'Hello World';
+console.log(str1)
+
+console.log(['Hello']);
+console.log(['World']); 
+
+
+var car ={
+    make: 'Lexus',
+    model: 'NX',
+    year: '2017',
+};
+console.log(car);
+console.log(car.make);
+console.log(car.model);
