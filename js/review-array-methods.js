0dc428fe64a471a3779588e0b8e2c535219d0d07
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-25 13:34:42 -0700

    arrays method

--- js/review-array-methods.js
+++ js/review-array-methods.js
@@ -1,4 +1,21 @@
-var fruit = ["apple", "strawberry","mango"];
-console.log[fruit];
-console.log[fruit [0]];
-console.log["fruitnnnn"];
+var fruit = ['apple', 'strawberry','mango'];
+console.log(fruit);
+
+var fruit1 = "apple";
+var fruit2 = "strawberry";
+var fruit3 = "mango";
+
+console.log(fruit[fruit.length-1]);
+fruit.push('peach');
+console.log(fruit);
+
+fruit.unshift('banana');
+console.log(fruit);
+
+fruit[2] = 'watermelon';
+
+fruit.splice(1, 1);
+
+var snacks = fruit;
+console.log(snacks);
+console.log(fruit);
